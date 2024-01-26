d4885b85f58031374fd0a54740e421f1f05996b3
BajanVlogs <kjolly544@gmail.com>
2024-01-25 23:30:52 -0500

    Create RedstoneIntegration.php

---
+++ src/RedStone/RedstoneIntegration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace YourNamespace;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockRedstoneEvent;
+
+class RedstoneIntegration extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onRedstoneChange(BlockRedstoneEvent $event) {
+        // Handle redstone changes here
+        $block = $event->getBlock();
+        $newPower = $event->getNewPower();
+
+        // Your custom redstone logic goes here
+
+        // Example: Broadcast a message when redstone changes
+        $this->getServer()->broadcastMessage("Redstone at {$block->getX()}, {$block->getY()}, {$block->getZ()} changed to {$newPower}");
+    }
+}
